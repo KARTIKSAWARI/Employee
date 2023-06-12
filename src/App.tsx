71bb7adf833353e0877c50342e3c6421fde6d0f9
Kartik Awari <kartikawari123@gmail.com>
2023-06-12 18:44:14 +0530

    New text fileds are added

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@
 // import logo from './logo.svg';
 import './App.css';
 import { Button } from '@mui/material';
+import Receiving from './Components/receiving';
 
 function App() {
   return (
     <div className="App">
       <h1>Kartik</h1>
       <Button variant="contained">Contained</Button>
-      =
+      <Receiving/>
     </div>
   );
 }
--- src/Components/receiving.tsx
+++ src/Components/receiving.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField'
 
 function receiving() {
   return (
@@ -11,12 +12,17 @@ function receiving() {
       padding={2} 
       width={1000}
       height={150}
-      
     >
-     
+    <div style={{display:'flex',alignItems:'center'}}> 
+     <TextField id="outlined-basic" name='Required' label="EmpId" variant="outlined" style={{margin:'10px'}}/>
+     <TextField id="outlined-basic" name='Required' label="EmpName" variant="outlined" style={{margin:'10px'}}/>
+     <TextField id="outlined-basic" name='Required' label="designation" variant="outlined" style={{margin:'10px'}}/>
+     <TextField id="outlined-basic" name='Required' label="Department" variant="outlined" style={{margin:'10px'}} />
+     <TextField id="outlined-basic" name='Required' label="Project" variant="outlined" style={{margin:'10px'}}/>
+    
+     </div>
 
 
-     
     </Box>
     </div>
   )
