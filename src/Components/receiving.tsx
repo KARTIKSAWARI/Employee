189ede4002051234de4281c5f03e33df6e2801a9
Kartik Awari <kartikawari123@gmail.com>
2023-06-12 18:17:11 +0530

    New Commit

---
+++ src/Components/receiving.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Box from '@mui/material/Box';
+
+function receiving() {
+  return (
+    <div>
+
+<Box
+      boxShadow={3} 
+      borderRadius={8} 
+      padding={2} 
+      width={1000}
+      height={20}
+    >
+      {/* Content inside the Box */}
+    </Box>
+    </div>
+  )
+}
+
+export default receiving;
