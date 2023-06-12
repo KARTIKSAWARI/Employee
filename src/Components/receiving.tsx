ea661b6ede9695151831bdb8fa3ecdd10491ff30
Kartik Awari <kartikawari123@gmail.com>
2023-06-12 18:27:00 +0530

    new Box created

--- src/Components/receiving.tsx
+++ src/Components/receiving.tsx
@@ -3,16 +3,20 @@ import Box from '@mui/material/Box';
 
 function receiving() {
   return (
-    <div>
+    <div style={{display:'flex',justifyContent:'center'}}>
 
 <Box
       boxShadow={3} 
       borderRadius={8} 
       padding={2} 
       width={1000}
-      height={20}
+      height={150}
+      
     >
-      {/* Content inside the Box */}
+     
+
+
+     
     </Box>
     </div>
   )
